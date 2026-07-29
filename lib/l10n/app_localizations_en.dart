2eea405350c1a814b496e665f8f8f13e3808fc05
LordRoboter <lordroboter@outlook.de>
2026-07-29 18:59:53 +0200

    Translations setup

---
+++ lib/l10n/app_localizations_en.dart
@@ -0,0 +1,25 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get helloWorld => 'Hello World!';
+
+  @override
+  String get home => 'Home';
+
+  @override
+  String get newEntries => 'New Entries!';
+
+  @override
+  String get plan => 'Plan';
+
+  @override
+  String get substPlan => 'Substitution Plan';
+}
